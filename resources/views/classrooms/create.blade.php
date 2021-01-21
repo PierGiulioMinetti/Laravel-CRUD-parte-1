220c91600a3739049342c7d645c15defec4a899d
Pier Giulio Minetti <pg.minetti@gmail.com>
2021-01-21 20:59:24 +0100

    Form Created

--- resources/views/classrooms/create.blade.php
+++ resources/views/classrooms/create.blade.php
@@ -4,4 +4,22 @@
     <h1>
         CREATE NEW CLASS
     </h1>
+    <form action="" method="POST">
+        @csrf
+        @method('POST')
+        
+        <div class="form-group">
+            <label for="name">Classroom name</label>
+            <input class="form-control" type="text" name="name">
+        </div>
+        
+        <div class="form-group">
+            <label for="name">Classroom description</label>
+            <textarea class="form-control" type="text" name="description"> </textarea>
+        </div>
+        <div class="form-group">
+            
+            <input class="btn btn-primary" type="submit" value="Create"> 
+        </div>
+    </form>
 @endsection
